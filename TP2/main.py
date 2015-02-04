9c1e820e6b73a15e9455524be2e2023dc8389866
elMathos <mathurin.massias@gmail.com>
2015-02-04 21:10:43 +0100

    Code for submission

--- TP2/main.py
+++ TP2/main.py
@@ -10,12 +10,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import KernelPCA
 from sklearn.metrics.pairwise import pairwise_kernels
+import scipy.optimize as opt
+
 
 X = np.loadtxt("zip.train")
 # dim 257, first line is the digit, we don't need it
 # we only keep the first 1000 points
-
-X = X[:, 1:]
+X = X[:1000, 1:]
 # let's plot a digit to see :
 plt.imshow(X[0].reshape((16, 16)), cmap=plt.cm.gray)
 
@@ -32,16 +33,15 @@ kpca_rbf = KernelPCA(kernel="rbf")
 X_kpca_rbf = kpca_rbf.fit_transform(X)
 
 # Plot results for first 200 points:
-nb_points = 200
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 3, 1, aspect='equal')
-plt.scatter(X_kpca_lin[:nb_points, 0], X_kpca_lin[:nb_points, 1])
+plt.scatter(X_kpca_lin[:200, 0], X_kpca_lin[:200, 1])
 plt.title("Linear kernel")
 plt.subplot(1, 3, 2, aspect='equal')
-plt.scatter(X_kpca_poly[:nb_points, 0], X_kpca_poly[:nb_points, 1])
+plt.scatter(X_kpca_poly[:200, 0], X_kpca_poly[:200, 1])
 plt.title("Polynomial kernel")
 plt.subplot(1, 3, 3, aspect='equal')
-plt.scatter(X_kpca_rbf[:nb_points, 0], X_kpca_rbf[:nb_points, 1])
+plt.scatter(X_kpca_rbf[:200, 0], X_kpca_rbf[:200, 1])
 plt.title("Gaussian kernel")
 plt.show()
 
@@ -76,52 +76,17 @@ def compute_gamma(y, X, d, std_dev):
         print str(i)
     return (gamma+1./dim)
 
-
-X_short = X[:200, :]
-gam = compute_gamma(noisy_img, X_short, 50, 10)
-
-
-
-
-
 std_dev = 10
+gam = compute_gamma(noisy_img, X, 50, std_dev)
+
 def function_to_optim(y):
     s = 0
     for i in range(199): 
         s -= gam[i]*np.exp(-np.linalg.norm(X[i]-y)**2/(2*std_dev**2))
     return s
 
-test = function_to_optim(noisy_img)
 
-import scipy.optimize as opt
-u = opt.minimize(function_to_optim, x0=X[3])
+u = opt.minimize(function_to_optim, x0=X[18]) # random starting point
 denoised = u["x"]
 plt.imshow(denoised.reshape((16, 16)), cmap=plt.cm.gray)
 
-
-
-
-
-def denoising_gamma(gamma, X, n_iter):
-    n_samples = len(X)
-    n_feat = len(X[0])
-    dim = len(gamma)
-    y = X[np.random.randint(0, n_samples)]  # initialization
-    L_y = [y]
-    for i in range(n_iter):
-        K_img = pairwise_kernels(X, y, metric="rbf")
-        K_img = K_img.reshape(n_samples)
-        numer = np.zeros(n_feat)
-        denom = 0
-        for j in range(dim):
-            numer += gamma[j]*K_img[j]*X[j]
-            denom += gamma[j]*K_img[j]
-        y = numer/denom
-        L_y . append(y)
-
-    return L_y
-    
-
-def denoising(noisy_img, X, d, std_dev, n_iter):
-    gamma = compute_gamma(noisy_img, X, d, std_dev)
-    return denoising_gamma(gamma, X, std_dev, n_iter)
